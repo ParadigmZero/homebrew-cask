b4da4db54a4cc7aa8f891abd562521860a8dc028
ParadigmZero <josephrcalton@gmail.com>
2025-05-08 11:48:29 +0100

    fix style offenses

--- Casks/o/okular.rb
+++ Casks/o/okular.rb
@@ -4,9 +4,8 @@ cask "okular" do
   version "6171"
   sha256 arm:   "a6cf172357c6746f3cde014087d85ccd8ad62e01e24ee7258851c2f296d14954",
          intel: "160f86c075e2340cefc9b9421e6b5607451d663ce3c29deb6a241ecf1c2ef71e"
-         
-  url "https://cdn.kde.org/ci-builds/graphics/okular/master/macos-arm64/okular-master-#{version}-macos-clang-#{arch}.dmg"
 
+  url "https://cdn.kde.org/ci-builds/graphics/okular/master/macos-arm64/okular-master-#{version}-macos-clang-#{arch}.dmg"
   name "Okular"
   desc "Document viewer"
   homepage "https://okular.kde.org/"
@@ -20,6 +19,6 @@ cask "okular" do
 
   zap trash: [
     "~/Library/Application Support/okular",
-    "~/Library/Preferences/org.kde.okular.plist"
+    "~/Library/Preferences/org.kde.okular.plist",
   ]
 end
